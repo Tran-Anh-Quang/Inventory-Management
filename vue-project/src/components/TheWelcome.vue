6d5c88e19dcdef3b0f2ed414c919e2bb2a038b94
Tran Anh Quang <dinosaurous9x@gmail.com>
2025-03-05 01:15:19 +0700

    dynamic value

---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,21 @@
+<script setup>
+const hello = 'VueJS'
+console.log(hello)
+
+const object = {
+  name: 'John',
+  age: 30
+}
+</script>
+
+<template>
+  <h1>This is {{ object.name }} {{ object.age }}</h1>
+</template>
+
+<style>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+</style>
